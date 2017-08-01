784ee4b9154a97117df6f43ff02ced9d02480dd0
kcbighuge <kcbighuge@gmail.com>
2017-08-01 12:43:20 -0700

    reverse sign of steering angle, increase cost function coeffs

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -56,21 +56,21 @@ class FG_eval {
     // TO_DID: Define the cost related the reference state and
     // any anything you think may be beneficial.
     for (int t=0; t<N; t++) {
-      fg[0] += 400*CppAD::pow(vars[cte_start+t], 2);
-      fg[0] += 400*CppAD::pow(vars[epsi_start+t], 2);
+      fg[0] += 4000*CppAD::pow(vars[cte_start+t], 2);
+      fg[0] += 4000*CppAD::pow(vars[epsi_start+t], 2);
       fg[0] += CppAD::pow(vars[v_start+t] - ref_v, 2);
     }
 
     // Actuator Cost
     for (int t=0; t < N-1; t++) {
-      fg[0] += CppAD::pow(vars[delta_start+t], 2);
-      fg[0] += CppAD::pow(vars[a_start+t], 2);
+      fg[0] += 10*CppAD::pow(vars[delta_start+t], 2);
+      fg[0] += 10*CppAD::pow(vars[a_start+t], 2);
     }
 
     // Value gap between sequential Actuator Cost
     for (int t=0; t < N-2; t++) {
-      fg[0] += 40*CppAD::pow(vars[delta_start+t+1] - vars[delta_start+t], 2);
-      fg[0] += 2*CppAD::pow(vars[a_start+t+1] - vars[a_start+t], 2);
+      fg[0] += 400*CppAD::pow(vars[delta_start+t+1] - vars[delta_start+t], 2);
+      fg[0] += 20*CppAD::pow(vars[a_start+t+1] - vars[a_start+t], 2);
     }
 
     //
